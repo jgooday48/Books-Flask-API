3726831cf9fb65e6d04b26ebedf9e079b17a8a1c
jgooday48 <joelgooday48@gmail.com>
2024-01-05 08:38:27 +0000

    fix app context bug

--- seed.py
+++ seed.py
@@ -1,6 +1,9 @@
-from application import db
+from application import create_app, db
 from application.books.models import Book
 from application.authors.models import Author
+
+app = create_app()
+app.app_context().push()  # push the app context
 
 db.drop_all()
 print("Dropping Database")
@@ -10,16 +13,6 @@ print("Creating Database")
 
 print("Seeding Database")
 
-# entry1 = Book(title="The Hobbit", author="J.R.R Tolkein")
-# entry2 = Book(title="American Psycho", author="Bret Easton Ellis")
-# entry3 = Book(title="Diary of a Wimpy Kid", author="Jeff Kinney")
-# entry4 = Book(title="Harry Potter and the Prisoner of Azkaban", author="J.K Rowling")
-
-# db.session.add(entry1)
-
-# db.session.add_all([entry2, entry3, entry4])
-
-# db.session.commit()
 
 author1 = Author(name="J.K. Rowling")
 author2 = Author(name="J.R.R. Tolkein")
@@ -34,5 +27,12 @@ book2 = Book(title="Harry Potter and the Chamber of Secrets", author_id=author1.
 book3 = Book(title="The Hobbit", author_id=author2.id)
 book4 = Book(title="The Rules of Atrraction", author_id=author3.id)
 book5 = Book(title="Less Than Zero", author_id=author3.id)
-db.session.add_all([book1, book2,book3,book4, book5])
+book6 = Book(title="American Psycho", author_id=author3.id)
+book7 = Book(title="Glammorama", author_id=author3.id)
+book8 = Book(title="Harry Potter and the Prisoner of Azkaban", author_id=author1.id)
+book9 = Book(title="Harry Potter and the Goblet of Fire", author_id=author1.id)
+book10 = Book(title="Harry Potter and the Order of the Phoneix", author_id=author1.id)
+book11 = Book(title="Harry Potter and the Half Blood Prince", author_id=author1.id)
+book12 = Book(title="Harry Potter and the Deathly Hallows", author_id=author1.id)
+db.session.add_all([book1, book2,book3,book4, book5,book6,book7,book8,book9,book10,book11,book12])
 db.session.commit()
